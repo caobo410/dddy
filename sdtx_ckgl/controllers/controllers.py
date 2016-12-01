764b75ab50a8333a145464d3f20ae7f739d819a6
xiaocao <caobo410@163.com>
2016-12-01 18:05:22 +0800

    删除没用的

--- sdtx_ckgl/controllers/controllers.py
+++ sdtx_ckgl/controllers/controllers.py
@@ -3,40 +3,40 @@ from openerp import http, fields,api
 import rest
 import authorizer
 from datetime import datetime
+import random
+import json
 
 try:
     import cStringIO as StringIO
 except ImportError:
     import StringIO
 
-
 date_ref = datetime.now().strftime('%Y-%m-%d')
 # _logger = logging.getLogger(__name__)
-
 class OrderController(http.Controller):
     @authorizer.authorize
-    @http.route('/api/sdtx_ckgl/<database>', type='http', auth='none', methods=['POST'])
+    @http.route('/api/sdtx_ckgl/<database>', type='http', auth='none', methods=['GET'])
     def sdtx_ckgl(self, database, login, password, type, warehouse_list, line_list):
         if type:
-            if type == u'xsck':
+            if type == u'8':
                 name = u'销售出库单'
                 print_name = u'sdtx_ckgl_report.report_sdtx_xsck_main'
-            elif type == u'xsth':
+            elif type == u'9':
                 name = u'销售退货单'
                 print_name = u'sdtx_ckgl_report.report_sdtx_xsth_main'
-            elif type == u'cgrk':
+            elif type == u'1':
                 name = u'采购入库单'
                 print_name = u'sdtx_ckgl_report.report_sdtx_cgrk_main'
-            elif type == u'cgth':
+            elif type == u'2':
                 name = u'采购退货单'
                 print_name = u'sdtx_ckgl_report.report_sdtx_cgth_main'
-            elif type == u'dbd':
+            elif type == u'5':
                 name = u'调拨单'
                 print_name = u'sdtx_ckgl_report.report_sdtx_dbd_main'
-            elif type == u'qtck':
+            elif type == u'3':
                 name = u'其他出库单'
                 print_name = u'sdtx_ckgl_report.report_sdtx_qtck_main'
-            elif type == u'qtrk':
+            elif type == u'4':
                 name = u'其他入库单'
                 print_name = u'sdtx_ckgl_report.report_sdtx_qtrk_main'
             else:
@@ -48,8 +48,10 @@ class OrderController(http.Controller):
         if not line_list:
             return rest.render_json({'status': 'no', 'message': name + u'商品明细不能为空！'})
         try:
-            warehouse_values = eval(warehouse_list)
-            line_values = eval(line_list)
+            warehouse_list = warehouse_list.replace('\"', '\'')
+            line_list = line_list.replace('\"', '\'')
+            warehouse_values = eval(warehouse_list.decode('raw_unicode_escape'))
+            line_values = eval(line_list.decode('raw_unicode_escape'))
         except:
             return rest.render_json({'status': 'no', 'message': 'warehouse_values/line_values参数有问题！'})
         self.current_env.cr.execute('delete from ckgl_dddy;delete from dddy_line')
@@ -64,4 +66,7 @@ class OrderController(http.Controller):
         report_objs = self.current_env['report']
         ckgl_objs = sdtx_ckgl_obj.search([])
         values = report_objs.get_pdf(ckgl_objs, print_name)
-        return rest.render_pdf(values, type)
+        sjs = str(random.randint(100, 999))
+        str_time = str(datetime.now())
+        filename = str_time[:4] + str_time[5:7] + str_time[8:10] + str_time[11:13] + str_time[14:16] + str_time[17:18] + type + sjs
+        return rest.render_pdf(values, filename)
